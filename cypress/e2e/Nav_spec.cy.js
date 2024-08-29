849316bcb5309d4ec358dac82ed23a93f8f178ba
ROlearyPro <rolearyprogramming@gmail.com>
2024-08-29 13:21:34 -0600

    implemented cypress test for filter by rating

--- cypress/e2e/Nav_spec.cy.js
+++ cypress/e2e/Nav_spec.cy.js
@@ -1,23 +1,25 @@
 describe('Nav spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
-  })
-  it('Should display the name of our application', () => {
-
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies').as('getMovies');
-
-    // Visit the page after the intercept is set up
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
+  });
+    it('passes', () => {
+      cy.visit('http://localhost:3000')
+    })
+    it('Should correctly display the webpage, or at least the nav bar portion', () => {
 
-    // Wait for the intercept alias to be called
-    cy.wait('@getMovies').as('moviesRequest'); // Alias the wait result
-    cy.get('h1').contains('Crusty')
-    cy.get('h1').contains('Carrots')
-    cy.get('img').should('have.attr', 'src', '/orange-carrot-2.svg')
-  })
-  it('Should have a search bar area', () => {
-    // Visit the page after the intercept is set up
-    cy.visit('http://localhost:3000');
+      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies').as('getMovies');
+
+      // Visit the page after the intercept is set up
+      cy.visit('http://localhost:3000');
+
+      // Wait for the intercept alias to be called
+      cy.wait('@getMovies').as('moviesRequest'); // Alias the wait result
+      cy.get('h1').contains('Crusty')
+      cy.get('h1').contains('Carrots')
+      cy.get('img').should('have.attr', 'src', '/orange-carrot-2.svg')
+    })
+    it('Should have a functioning input area', () => {
+      // Visit the page after the intercept is set up
 
       // Check that the search bar can take input; currently doesn't check that.
       cy.get('.search-by-title').type('Adam');
@@ -25,11 +27,16 @@ describe('Nav spec', () => {
 
 
     });
-  it('Should have dropdown filters', () => {
+    it('Should have dropdown filters', () => {
 
-    cy.visit('http://localhost:3000')
-    cy.get('.filter-by-genre').select('test').invoke("val").should("eq", "test");
-  })
+      cy.get('.filter-by-rating').select('8').invoke("val").should("eq", "8");
+    })
+    it('Should have functioning dropdown filters', () => {
+      cy.get('.filter-by-rating').select('8').invoke("val").should("eq", "8");
+      cy.get('.movie-title').contains("Black Adam").should('not.exist');
+      cy.get('.movie-title').contains("Smile").should('exist');
+    })
+
   });
 // .get('h1').contains('h1', 'Feedback Loop') //
 // As a user, when I load the application, I can see the title of the application
